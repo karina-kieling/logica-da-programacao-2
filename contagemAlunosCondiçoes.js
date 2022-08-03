88bac0384b074276e94b23dbe815b8b0296521ca
karina kieling <karinagabrielekieling@gmail.com>
2022-08-03 15:14:54 +0100

    praticas

---
+++ contagemAlunosCondiçoes.js
@@ -0,0 +1,42 @@
+//let index = 0 - declaracao de variavel contadora
+//index < array.length - verificação da quantidade/quantas voltas ira repetir
+//index++ - incremento/ira adicionar a cada volta
+//length - comprimento
+//array - lista
+
+
+for (let index = 0; index < array.length; index++) {
+        
+}
+
+//forEach - especifico para listas/array
+array.array.forEach(element => {
+    
+});
+
+//while - estrutura de repetição condicional, verifica a condição ANTES da execução...
+//condição sempre deve dar um resultado verdadeiro pois se for falso não ira executar
+while (condition) {
+    
+}
+
+//do while - estrutura de repetição condicional, verifica a condição DEPOIS da primeira execução...
+//condição vai executar pelo menos 1 vez mesmo se informação for falsa 
+do {
+    
+} while (condition);
+
+// texto - string...precisa estar entre aspas
+// numeros inteiros - int
+// numeros decimais - float 0,000000
+// numero decimais - double 0,0000000000000000
+// 0 ou 1 / false ou true - bool ou boolean
+
+//exemplo:
+
+let quantidade = 5
+
+for (let contador = 0; contador < quantidade; contador++) {
+    console.log("o numero da volta é " + (contador + 1))
+    
+}
